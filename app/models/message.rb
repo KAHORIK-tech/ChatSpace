acbf188e946f9029fdd692eb4187498ba3911aeb
KAHORIK-tech <akikutu75@gmail.com>
2020-04-28 14:27:02 +0900

    メッセージ送信機能実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
-class Messege < ApplicationRecord
+class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
   validates :body, presence: true, unless: :image?
 
-  mount_uploader image, ImageUploder
+  mount_uploader :image, ImageUploader
 end
--- db/migrate/20200428024602_create_messages.rb
+++ db/migrate/20200428024602_create_messages.rb
@@ -1,6 +1,6 @@
-class CreateMesseges < ActiveRecord::Migration[5.0]
+class CreateMessages < ActiveRecord::Migration[5.0]
   def change
-    create_table :messeges do |t|
+    create_table :messages do |t|
       t.text :body
       t.string :image
       t.references :user, foreign_key: true
