faa4a372edf505a10772080bf2b5f2f4373053d7
KAHORIK-tech <akikutu75@gmail.com>
2020-05-11 12:02:13 +0900

    3

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.14.0'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:KAHORIK-tech/ChatSpace.git'
+set :repo_url,  'https://github.com/KAHORIK-tech/ChatSpace'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
